324dd7337210778041c9b2e9435b041bb2d90248
gilberto <marcanogilbertoa@gmail.com>
2020-12-15 18:17:51 -0400

    Commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gnstagram.wsgi --log-file -
--- manage.py
+++ manage.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
+<<<<<<< HEAD
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'platzigram.settings')
+=======
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gnstagram.settings')
+>>>>>>> 986957d (Commit)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

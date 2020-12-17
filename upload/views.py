c2884cac728c657a89ee44f859f5093fb2064f64
gilberto <marcanogilbertoa@gmail.com>
2020-12-17 14:29:00 -0400

    Upload test

--- upload/views.py
+++ upload/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.shortcuts import redirect
 
@@ -7,19 +8,21 @@ from posts.forms import PostForm
 from posts.models import Post
 
 # Create your views here.
+@login_required
 def avatar_upload(request):
     user = request.user
     instance = UserProfile.objects.get(pk=1)
-    print('Epa update')
     if request.method == "POST":
         # form = AvatarUploadForm(request.POST, request.FILES, instance=instance)
-        form = AvatarUploadForm(request.POST, request.FILES, instance=instance)
-        print(form)
-
+        form = PostForm(request.POST, request.FILES, instance=instance)
         if form.is_valid():
             form.save()
-            return redirect('upload/')
+            return redirect('posts:feed')
     else:
-        print('GET REQUESTTTTTT')
-    form = PostForm()
-    return render(request, 'avatar_upload.html', {'form': form})
+        form = PostForm()
+    
+    return render(
+        request,
+        'avatar_upload.html',
+        {'form': form}
+    )
